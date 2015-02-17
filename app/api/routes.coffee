603d5d146bdfee7fecc571abe39bd4765dfd9862
Neil Carpenter <neilanthonycarpenter@gmail.com>
2015-02-17 01:43:23 +0000

    basic API, just reading doodle dir for now

--- app/api/routes.coffee
+++ app/api/routes.coffee
@@ -1,10 +1,16 @@
+doodleData = require '../../utils/getDoodleData'
 
 getDoodles = (req, res) ->
 
-	res.json doodles : ['sup']
+	res.json doodleData.getDoodles()
+
+getContributors = (req, res) ->
+
+	res.json doodleData.getContributors()
 
 setup = (app) ->
 
 	app.get '/api/doodles', getDoodles
+	app.get '/api/contributors', getContributors
 
 module.exports = setup
---
+++ utils/getDoodleData.js
@@ -0,0 +1,61 @@
+// config is coffee....
+require('coffee-script/register');
+
+var _      = require('underscore');
+var fs     = require('fs');
+var config = require('../config/doodles');
+ 
+function getDoodles() {
+
+  var doodles = [];
+
+  fs.readdirSync('doodles').forEach(function(authorPath, i) {
+
+    authorPath = 'doodles/' + authorPath;
+
+    if (fs.lstatSync(authorPath).isDirectory()) {
+
+      fs.readdirSync(authorPath).forEach(function(doodlePath, i) {
+
+        var manifestPath;
+
+        doodlePath   = authorPath + '/' + doodlePath;
+        manifestPath = doodlePath+'/manifest.json';
+
+        if (fs.lstatSync(doodlePath).isDirectory()) {
+
+          if (fs.existsSync(manifestPath)) {
+            doodles.push(JSON.parse(fs.readFileSync(manifestPath, {encoding: 'utf8'})));
+          } else {
+            console.log('\033[31mNo manifest.json found for doodle : '+doodlePath+'\033[0m');
+          }
+
+        }
+
+      });
+
+    }
+
+  });
+
+  return doodles;
+
+}
+
+function getContributors() {
+
+  var authorsUniq = [];
+
+  var authors = _.pluck(getDoodles(), 'author');
+  authors = _.groupBy(authors, function(author) { return author.name; });
+
+  _.each(authors, function(author) { authorsUniq.push(author[0]); })
+
+  return authorsUniq;
+
+}
+
+module.exports = {
+  getDoodles      : getDoodles,
+  getContributors : getContributors
+};
